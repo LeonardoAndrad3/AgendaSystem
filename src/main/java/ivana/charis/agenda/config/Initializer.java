648ef42a7306e9a0a1e4055b4b6afb1ba9c4c348
leonardoAndard3 <leonardoxcontato@gmail.com>
2024-02-15 22:43:44 -0300

    Changer database to mysql

--- src/main/java/ivana/charis/agenda/config/Initializer.java
+++ src/main/java/ivana/charis/agenda/config/Initializer.java
@@ -35,7 +35,7 @@ public class Initializer implements CommandLineRunner {
 //        eRep.deleteAll();
 //        sRep.deleteAll();
 
-        Endereco endereco = new Endereco("Rua ABC", "Centro", "12345-678", (short) 123, "Apto 101", "São Paulo", "SP");
+        Endereco endereco = new Endereco("Rua ABC", "Centro", "12345-678", (short) 223, "Apto 101", "São Paulo", "SP");
         Endereco endereco2 = new Endereco("Avenida XYZ", "Vila Nova", "54321-987", (short) 456, "Casa 202", "Rio de Janeiro", "RJ");
         var e = new Employee(null, "Mariana", "maria@gmail.com", "(11) 96012-2205", "/photos/storage/maria.png", "Sou zika", Work.MANICURE, new ArrayList<>(), endereco);
         var c = new Client(null, "Marcia", "marcia@gmail.com", "(11) 96041-2305", "/photos/storage/marcia.png", new ArrayList<>(), endereco2);
--- src/main/java/ivana/charis/agenda/infra/security/SecurityConfig.java
+++ src/main/java/ivana/charis/agenda/infra/security/SecurityConfig.java
@@ -11,12 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
 
-
+        return null;
     }
 
 }
